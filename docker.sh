82791f44cafccd51aaf97e47589605bb7fd802df
cambo <cambo@bure.com.au>
2022-11-03 14:23:56 +1100

    update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,7 +2,7 @@
 
 aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 076162133364.dkr.ecr.ap-southeast-2.amazonaws.com
 
-docker build -t bureaunext .
+docker buildx build --platform=linux/amd64 -t bureaunext .
 
 docker tag bureaunext:latest 076162133364.dkr.ecr.ap-southeast-2.amazonaws.com/bureaunext:latest
 
