b02378937b37222b9684fd2a05842c08cb09f3db
cambo <cambo@bure.com.au>
2023-11-04 17:02:13 +1100

    update readme.md; remove aws-specific deploy files

--- docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 076162133364.dkr.ecr.ap-southeast-2.amazonaws.com
-
-docker buildx build --platform=linux/amd64 -t bureaunext .
-
-docker tag bureaunext:latest 076162133364.dkr.ecr.ap-southeast-2.amazonaws.com/bureaunext:latest
-
-docker push 076162133364.dkr.ecr.ap-southeast-2.amazonaws.com/bureaunext:latest
